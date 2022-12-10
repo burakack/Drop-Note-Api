98cf6362b7160ccce664af817bd42a86f660d0ff
burakack <burak_ack@hotmail.com>
2022-12-10 06:10:59 +0300

    dynamic migration system

--- src/migrate.js
+++ src/migrate.js
@@ -5,22 +5,41 @@ const db = require("./database");
 //add to last index of the array!
 var migratefiles = new Array("./migrations/202206231018-createdatabase.sql");
 //running sql files
-for (i = 0; i < migratefiles.length; i++) {
-  var sql = fs
-    .readFileSync(
-      migratefiles[i],
-      { encoding: "utf8", flag: "r" },
-      function (err, data) {
-        if (err) console.log(err);
-      }
-    )
-    .toString();
 
-  db.query(sql, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      process.exit(1);
-    }
-  });
-}
+fs.readdirSync("./migrations").map((file) => {
+  console.log(file);
+  let sql=fs.readFileSync(
+    "./migrations/" + file.toString(),
+    { encoding: "utf8", flag: "r" },
+    function (err, data) {
+      if (err) console.log(err);
+    }).toString();
+
+
+      db.query(sql, (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          process.exit(1);
+        }
+      });
+});
+
+// for (i = 0; i < migratefiles.length; i++) {
+//   var sql = fs
+//     .readFileSync(
+//       migratefiles[i],
+//       { encoding: "utf8", flag: "r" },
+//       function (err, data) {
+//         if (err) console.log(err);
+//       }
+//     )
+//     .toString();
+
+// db.query(sql, (err, res) => {
+//   if (err) {
+//     console.log("error: ", err);
+//     process.exit(1);
+//   }
+// });
+// }
 console.log("Database migrated succesfully!");
