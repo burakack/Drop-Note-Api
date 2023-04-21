c7f9bea58c2638f1a48a62006c86ee746fc4c132
burakack <burak_ack@hotmail.com>
2023-04-22 02:30:12 +0300

    unique user

--- src/seed.js
+++ src/seed.js
@@ -1,15 +1,32 @@
-const db = require("./database.js")
-
+const db = require("./database.js");
 
 const insertUserQuery = `INSERT INTO users (nickname, email, password_salt, password_hash, created_At)
                           VALUES ($1, $2, $3, $4, $5)`;
 
 const users = [
-  { nickname: 'John Doe', email: 'john@example.com', password_salt: 'password1',password_hash:'password1',created_At: '2021-01-01 00:00:00' },
-  { nickname: 'Jane Doe', email: 'jane@example.com', password_salt: 'password2',password_hash:'password2',created_At: '2021-01-01 00:00:00' }
+  {
+    nickname: `TESTUSER${Date.now()}+1`,
+    email: `abdaajı${Date.now()}@hotmail.com`,
+    password_salt: "password1",
+    password_hash: "password1",
+    created_At: "2021-01-01 00:00:00",
+  },
+  {
+    nickname: `TESTUSER${Date.now()}`,
+    email: `abdaajı${Date.now()}1@hotmail.com`,
+    password_salt: "password2",
+    password_hash: "password2",
+    created_At: "2021-01-01 00:00:00",
+  },
 ];
 
-users.forEach(user => {
-  const { nickname, email, password_salt,password_hash,created_At } = user;
-  db.query(insertUserQuery, [nickname, email, password_salt,password_hash,created_At]);
+users.forEach((user) => {
+  const { nickname, email, password_salt, password_hash, created_At } = user;
+  db.query(insertUserQuery, [
+    nickname,
+    email,
+    password_salt,
+    password_hash,
+    created_At,
+  ]);
 });
